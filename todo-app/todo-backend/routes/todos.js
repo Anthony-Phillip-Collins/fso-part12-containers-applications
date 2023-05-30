c799c240231d532cbc777aaf99f9335c8848175a
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-05-30 10:34:46 +1000

    Exercise 12.7: Little bit of MongoDB coding

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
@@ -12,38 +12,54 @@ router.get('/', async (_, res) => {
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
-
-  next()
-}
+  const { id } = req.params;
+  try {
+    req.todo = await Todo.findById(id);
+    if (!req.todo) return res.sendStatus(404);
+  } catch (e) {
+    return res.status(400).send('Malformed id!');
+  }
+  next();
+};
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const update = {};
+
+  Object.keys(req.body).forEach((key) => {
+    if (['text', 'done'].includes(key)) {
+      update[key] = req.body[key];
+    }
+  });
+
+  try {
+    await req.todo.update(update);
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+
+  res.send({ ...res.todo, ...update });
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use('/:id', findByIdMiddleware, singleRouter);
 
 module.exports = router;
